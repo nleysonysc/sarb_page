bc157b35d034f592a0210ce8508bf4a813d531f0
nleyson <nleyson@yescharteracademy.org>
2022-12-12 12:35:46 -0800

    adding style

--- src/components/StudentItem.vue
+++ src/components/StudentItem.vue
@@ -1,43 +1,41 @@
 <script setup>
-  import { ref } from 'vue'
+  import StudentReports from './StudentReports.vue'
+  import { ref, onMounted, toRefs } from 'vue'
 
-  defineProps({
+  const props = defineProps({
     student: Object
   })
 
+  const { student } = toRefs(props)
+
   const reports = ref([])
 
-  function maxLvl (student){
-    return student.absences < 10 ? Math.floor(student.absences/3) : 3
+  function maxLvl (){
+    return student.value.absences < 10 ? Math.floor(student.value.absences/3) : 3
   }
 
-  async function createLetter(student, lvl) {
+  async function createLetter(lvl) {
     await setTimeout(()=> {console.log('create letter '+lvl)}, 599)
-    fetchStudentReports(student)
+    fetchStudentReports(student.value)
   }
 
-  function fetchStudentReports(student) {
-    setTimeout(() => {reports.value = [{title: student.first_name+' report', id: '2234', url:"https://example.com"}]}, 499)
-    console.log(reports.value)
+  async function fetchStudentReports() {
+    setTimeout(() => {reports.value = [{title: student.value.first_name+' report', id: '2234', url:"https://example.com"}]}, 499)
   }
+
+  fetchStudentReports(student.value)
 
 </script>
 
 <template>
   <li class="student-item">
-    <div>
+    <div class="student-options">
       <span>Name: {{student.first_name}} {{student.last_name}}</span>
       <span>Absences: {{student.absences}}</span>
       <span>Absent Dates: {{student.absent_dates}}</span>
-      <button v-for="lvl in maxLvl(student)" @click="()=>{createLetter(student, lvl)}">Create Level {{lvl}}</button>
+      <button v-for="lvl in maxLvl()" @click="()=>{createLetter(lvl)}">Create Level {{lvl}}</button>
     </div>
-    <div class="reports">
-      <ul>
-        <li v-for="report in reports" :key="report.id">
-          <a :href="report.url">{{report.title}}</a>
-        </li>
-      </ul>
-    </div>
+    <StudentReports :studentReports="reports" />
   </li>
 </template>
 
@@ -50,5 +48,17 @@
 
   li:nth-child(even) {
     background-color: #dfdfdf;
+  }
+
+  button {
+    max-width: 25%;
+  }
+
+  li > div.student-options {
+    display:grid;
+  }
+
+  span:first-child {
+    font-weight: bold;
   }
 </style>
---
+++ src/components/StudentReports.vue
@@ -0,0 +1,26 @@
+<script setup>
+  defineProps({
+    studentReports: Array
+  })
+
+</script>
+
+<template>
+  <div class="reports">
+      <h3>Previous Letters</h3>
+      <ul>
+        <li v-for="report in studentReports" :key="report.id">
+          <a :href="report.url">{{report.title}}</a>
+        </li>
+      </ul>
+    </div>
+</template>
+
+<style scoped>
+  h3 {
+    margin: 0;
+    padding: 0;
+    font-size: 1.1em;
+  }
+  
+</style>
